1367e1d8bc4a473081dc93468ff9802a7801f63d
Habiba678 <hananhussain12@hotmail.com>
2026-06-07 22:59:56 +0200

    Build character movement animations and knockback

--- models/character.class.js
+++ models/character.class.js
@@ -1,84 +1,164 @@
 class Character extends MovableObject {
     world;
-    walking_sound = new Audio('audio/running.mp3');
+
+    x = 100;
+    y = 165;
+    width = 100;
     height = 250;
-    y = 80;
     speed = 10;
+
+    offset = {
+        top: 60,
+        bottom: 10,
+        left: 25,
+        right: 25
+    };
+
+    IMAGES_IDLE = [
+        "./assets/img/2_character_pepe/1_idle/idle/I-1.png",
+        "./assets/img/2_character_pepe/1_idle/idle/I-2.png",
+        "./assets/img/2_character_pepe/1_idle/idle/I-3.png",
+        "./assets/img/2_character_pepe/1_idle/idle/I-6.png",
+        "./assets/img/2_character_pepe/1_idle/idle/I-7.png",
+        "./assets/img/2_character_pepe/1_idle/idle/I-8.png",
+        "./assets/img/2_character_pepe/1_idle/idle/I-9.png",
+        "./assets/img/2_character_pepe/1_idle/idle/I-10.png"
+    ];
+
     IMAGES_WALKING = [
-        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-22.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-23.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-24.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-25.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-26.png'
+        "./assets/img/2_character_pepe/2_walk/W-21.png",
+        "./assets/img/2_character_pepe/2_walk/W-22.png",
+        "./assets/img/2_character_pepe/2_walk/W-23.png",
+        "./assets/img/2_character_pepe/2_walk/W-24.png",
+        "./assets/img/2_character_pepe/2_walk/W-25.png",
+        "./assets/img/2_character_pepe/2_walk/W-26.png"
     ];
+
     IMAGES_JUMPING = [
-        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-31.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-32.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-33.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-34.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-35.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-36.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-37.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-38.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-39.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-40.png'
+        "./assets/img/2_character_pepe/3_jump/J-31.png",
+        "./assets/img/2_character_pepe/3_jump/J-32.png",
+        "./assets/img/2_character_pepe/3_jump/J-33.png",
+        "./assets/img/2_character_pepe/3_jump/J-34.png",
+        "./assets/img/2_character_pepe/3_jump/J-35.png",
+        "./assets/img/2_character_pepe/3_jump/J-36.png",
+        "./assets/img/2_character_pepe/3_jump/J-37.png",
+        "./assets/img/2_character_pepe/3_jump/J-38.png",
+        "./assets/img/2_character_pepe/3_jump/J-39.png"
     ];
-    IMAGES_DEAD = [
-        'img/2.Secuencias_Personaje-Pepe-corrección/5.Muerte/D-51.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/5.Muerte/D-52.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/5.Muerte/D-53.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/5.Muerte/D-54.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/5.Muerte/D-55.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/5.Muerte/D-56.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/5.Muerte/D-57.png'
-    ];
+
     IMAGES_HURT = [
-        'img/2.Secuencias_Personaje-Pepe-corrección/4.Herido/H-41.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/4.Herido/H-42.png',
-        'img/2.Secuencias_Personaje-Pepe-corrección/4.Herido/H-43.png'
+        "./assets/img/2_character_pepe/4_hurt/H-41.png",
+        "./assets/img/2_character_pepe/4_hurt/H-42.png",
+        "./assets/img/2_character_pepe/4_hurt/H-43.png"
     ];
+
+    IMAGES_DEAD = [
+        "./assets/img/2_character_pepe/5_dead/D-51.png",
+        "./assets/img/2_character_pepe/5_dead/D-52.png",
+        "./assets/img/2_character_pepe/5_dead/D-53.png",
+        "./assets/img/2_character_pepe/5_dead/D-54.png",
+        "./assets/img/2_character_pepe/5_dead/D-55.png",
+        "./assets/img/2_character_pepe/5_dead/D-56.png",
+        "./assets/img/2_character_pepe/5_dead/D-57.png"
+    ];
+
     constructor() {
-        super().loadImage('img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png');
-        this.loadImages(this.IMAGES_WALKING);
-        this.loadImages(this.IMAGES_JUMPING);
-        this.loadImages(this.IMAGES_DEAD);
-        this.loadImages(this.IMAGES_HURT);
+        super();
+        this.loadImage(this.IMAGES_IDLE[0]);
+        this.loadCharacterImages();
         this.applyGravity();
-        this.animate(this.IMAGES_WALKING);
+        this.startMovement();
+        this.startAnimation();
     }
 
-    animate() {
-        setInterval(() => {
-            this.walking_sound.pause();
-            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-                this.moveRight();
-                this.otherDirection = false;
-                this.walking_sound.play();
-            }
-            if (this.world.keyboard.LEFT && this.x > 0) {
-                this.moveLeft();
-                this.otherDirection = true;
-                this.walking_sound.play();
-            }
-            if (this.world.keyboard.SPACE && !this.isAboveGround()) {
-                this.jump();
-            }
-            this.world.camera_x = -this.x + 100;
-        }, 1000 / 60)
+    /**
+     * Loads all character image groups.
+     */
+    loadCharacterImages() {
+        this.loadImages(this.IMAGES_IDLE);
+        this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
+        this.loadImages(this.IMAGES_HURT);
+        this.loadImages(this.IMAGES_DEAD);
+    }
 
+    /**
+     * Starts the movement loop.
+     */
+    startMovement() {
         setInterval(() => {
-            if (this.isDead()) {
-                this.playAnimation(this.IMAGES_DEAD);
-            } else if (this.isHurt()) {
-                this.playAnimation(this.IMAGES_HURT);
-            } else if (this.isAboveGround()) {
-                this.playAnimation(this.IMAGES_JUMPING);
-            } else {
-                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                    this.playAnimation(this.IMAGES_WALKING);
-                }
-            }
-        }, 50);
+            if (!this.world) return;
+
+            this.moveByKeyboard();
+            this.jumpByKeyboard();
+            this.updateCamera();
+        }, 1000 / 60);
+    }
+
+    /**
+     * Handles left and right movement.
+     */
+    moveByKeyboard() {
+        if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
+            this.moveRight();
+            this.otherDirection = false;
+        }
+
+        if (this.world.keyboard.LEFT && this.x > 0) {
+            this.moveLeft();
+            this.otherDirection = true;
+        }
+    }
+
+    /**
+     * Handles jump input.
+     */
+    jumpByKeyboard() {
+        if (this.world.keyboard.SPACE) {
+            this.jump();
+        }
+    }
+
+    /**
+     * Updates camera position.
+     */
+    updateCamera() {
+        this.world.camera_x = -this.x + 100;
+    }
+
+    /**
+     * Starts the animation loop.
+     */
+    startAnimation() {
+        setInterval(() => {
+            this.playCurrentAnimation();
+        }, 80);
+    }
+
+    /**
+     * Chooses the correct animation.
+     */
+    playCurrentAnimation() {
+        if (this.isDead()) {
+            this.playAnimation(this.IMAGES_DEAD);
+        } else if (this.isHurt()) {
+            this.playAnimation(this.IMAGES_HURT);
+        } else if (this.isAboveGround()) {
+            this.playAnimation(this.IMAGES_JUMPING);
+        } else if (this.isMoving()) {
+            this.playAnimation(this.IMAGES_WALKING);
+        } else {
+            this.playAnimation(this.IMAGES_IDLE);
+        }
+    }
+
+    /**
+     * Checks if the character is walking.
+     * @returns {boolean}
+     */
+    isMoving() {
+        if (!this.world) return false;
+
+        return this.world.keyboard.RIGHT || this.world.keyboard.LEFT;
     }
 }
