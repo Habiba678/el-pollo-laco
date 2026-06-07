fe4f69e43b7325677d76e17ee07137d7f472a8cf
Habiba678 <hananhussain12@hotmail.com>
2026-06-08 00:20:57 +0200

    Build first level with enemies clouds and background

--- js/levels/level1.js
+++ js/levels/level1.js
@@ -1,35 +1,79 @@
-const level1 = new Level(
-    [
+/**
+ * Creates the first game level.
+ * @returns {Level} The prepared first level.
+ */
+function createLevel1() {
+    return new Level(
+        buildLevelEnemies(),
+        buildLevelClouds(),
+        buildLevelBackground()
+    );
+}
+
+/**
+ * Creates all enemies for the first level.
+ * @returns {MovableObject[]} Enemies used in the level.
+ */
+function buildLevelEnemies() {
+    return [
+        new ChickenSmall(),
         new Chicken(),
+        new ChickenSmall(),
         new Chicken(),
+        new ChickenSmall(),
         new Chicken(),
         new Endboss()
-    ],
-    [
-        new Cloud()
-    ],
-    [
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', -719),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', -719),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', -719),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', -719),
+    ];
+}
 
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 0),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 0),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 0),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 0), 
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 719),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 719),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 719),
+/**
+ * Creates all cloud objects.
+ * @returns {Cloud[]} Clouds used in the level.
+ */
+function buildLevelClouds() {
+    return [
+        new Cloud(-200),
+        new Cloud(250),
+        new Cloud(700),
+        new Cloud(1150),
+        new Cloud(1600),
+        new Cloud(2050),
+        new Cloud(2500)
+    ];
+}
 
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719 * 2),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 719 * 2),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 719 * 2),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 719 * 2),
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719 * 3),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 719 * 3),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 719 * 3),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 719 * 3),
+/**
+ * Creates all background images for the level.
+ * @returns {BackgroundObject[]} Background objects.
+ */
+function buildLevelBackground() {
+    return [
+        
+
+        new BackgroundObject('assets/img/5_background/layers/air.png', -719),
+        new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', -719),
+        new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', -719),
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', -719),
+
+        new BackgroundObject('assets/img/5_background/layers/air.png', 0),
+        new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 0),
+
+        new BackgroundObject('assets/img/5_background/layers/air.png', 719),
+        new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', 719),
+        new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', 719),
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', 719),
+
+        new BackgroundObject('assets/img/5_background/layers/air.png', 719 * 2),
+        new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 719 * 2),
+        new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 719 * 2),
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 719 * 2),
+
+        new BackgroundObject('assets/img/5_background/layers/air.png', 719 * 3),
+        new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', 719 * 3),
+        new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', 719 * 3),
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', 719 * 3)
     ]
-);
+
+}
