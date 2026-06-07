704a3929e91c617345daa8bbda5119f7064954ec
Habiba678 <hananhussain12@hotmail.com>
2026-06-07 23:16:04 +0200

    Build chicken enemy movement and defeat state

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,38 +1,67 @@
+/**
+ * Enemy chicken that walks through the level and can be defeated.
+ */
 class Chicken extends MovableObject {
+    x = 1000 + Math.random() * 500;
+    y = 360;
+    width = 120;
+    height = 70;
+    speed = 0.1 + Math.random() * 0.45;
+    isDefeated = false;
 
+    offset = {
+        top: 8,
+        bottom: 8,
+        left: 10,
+        right: 10
+    };
 
-    height = 55;
-    width = 70
-    y = 360
-    IMAGES_WALKING = [
-        'img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png',
-        'img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/2-Ga_centro.png',
-        'img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/3.Ga_paso izquierdo.png'
-
+    walkingImages = [
+        "./assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+        "./assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+        "./assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png"
     ];
 
+    defeatImage = "./assets/img/3_enemies_chicken/chicken_normal/2_dead/dead.png";
 
     constructor() {
-        super().loadImage('img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png');
-        this.loadImages(this.IMAGES_WALKING);
-
-        this.x = 200 + Math.random() * 500; // Zahl zwischen 200 und 700
-        this.speed = 0.15 + Math.random() * 0.5;
-
-        this.animate();
+        super();
+        this.loadImage(this.walkingImages[0]);
+        this.loadImages(this.walkingImages);
+        this.loadImages([this.defeatImage]);
+        this.applyGravity();
+        this.startChickenMovement();
+        this.startChickenAnimation();
     }
 
-
-
-    animate() {
+    /**
+     * Moves the chicken to the left while it is active.
+     */
+    startChickenMovement() {
         setInterval(() => {
-            this.moveLeft();
+            if (!this.isDefeated) {
+                this.moveLeft();
+            }
         }, 1000 / 60);
+    }
 
+    /**
+     * Plays the chicken walk animation while it is active.
+     */
+    startChickenAnimation() {
         setInterval(() => {
-            this.playAnimation(this.IMAGES_WALKING);
+            if (!this.isDefeated) {
+                this.playAnimation(this.walkingImages);
+            }
         }, 200);
     }
 
-
+    /**
+     * Stops the chicken and shows the defeated image.
+     */
+    defeat() {
+        this.isDefeated = true;
+        this.speed = 0;
+        this.img = this.imageCache[this.defeatImage];
+    }
 }
