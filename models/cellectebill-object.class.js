daaf1c0c40db51564a2edbc603b21117e67a3b0f
Habiba678 <hananhussain12@hotmail.com>
2026-06-08 19:12:41 +0200

    Refine player controls

--- models/cellectebill-object.class.js
+++ models/cellectebill-object.class.js
@@ -63,6 +63,6 @@ class CollectibleObject extends MovableObject {
     animateCoin() {
         setInterval(() => {
             this.playAnimation(this.coinImages);
-        }, 250);
+        }, 300);
     }
 }
