bcb894b062ddde6d8aee2cef64848ff8bb4ce9ae
Habiba678 <hananhussain12@hotmail.com>
2026-06-10 17:15:45 +0200

    Create game audio class

---
+++ models/audio.class.js
@@ -0,0 +1,11 @@
+/**
+ * Handles game audio in small controlled steps.
+ */
+class GameAudio {
+    constructor() {
+        this.muted = false;
+        this.unlocked = false;
+        this.musicTracks = {};
+        this.soundEffects = {};
+    }
+}
